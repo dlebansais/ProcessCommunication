7d0bd26a3139578ba6bb1cd16708bbe0cc6381e0
David Le Bansais <dlebansais@gmail.com>
2024-10-16 22:47:34 +0200

    Improve coverage.

--- Test/ProcessCommunication.Test/TestChannelChild.cs
+++ Test/ProcessCommunication.Test/TestChannelChild.cs
@@ -6,6 +6,9 @@ public class TestChannelChild(Guid guid, Mode mode) : Channel(guid, mode)
 {
     protected override void Dispose(bool disposing)
     {
+        // For coverage only. Validates the dispose pattern.
+        base.Dispose(false);
         base.Dispose(disposing);
+        base.Dispose(false);
     }
 }
